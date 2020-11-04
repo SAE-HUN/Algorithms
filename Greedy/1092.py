5f953a029fd738a98303055032a00dff8b956544
SAE-HUN <59135804+SAE-HUN@users.noreply.github.com>
2020-11-04 21:16:03 +0900

    배

--- Greedy/1092.py
+++ Greedy/1092.py
@@ -1,1 +1,46 @@
+import bisect
+import sys
+import collections
 
+n = int(input())
+cranes = list(map(int, input().split()))
+m = int(input())
+boxes = list(map(int, input().split()))
+
+_cranes = sorted(list(set(cranes)))
+box_cnt = [0] * len(_cranes)
+
+for box in boxes:
+  idx = bisect.bisect_left(_cranes, box)
+  if idx == len(_cranes):
+    print(-1)
+    sys.exit()
+  box_cnt[idx] += 1
+  
+crane_cnt = [x[1] for x in sorted(collections.Counter(cranes).items())]
+answer = 0
+
+while m>0:
+  for i in range(len(_cranes)-1, 0, -1):
+    if box_cnt[i] == 0:
+      crane_cnt[i-1] += crane_cnt[i]
+      crane_cnt[i] = 0
+    if box_cnt[i] >= crane_cnt[i]:
+      box_cnt[i] -= crane_cnt[i]
+      m -= crane_cnt[i]
+    else:
+      crane_cnt[i-1] += crane_cnt[i] - box_cnt[i]
+      crane_cnt[i] = box_cnt[i]
+      m -= box_cnt[i]
+      box_cnt[i] = 0
+  if box_cnt[0]:
+    if box_cnt[0] >= crane_cnt[0]:
+      box_cnt[0] -= crane_cnt[0]
+      m -= crane_cnt[0]
+    else:
+      m -= box_cnt[0]
+      box_cnt[0] = 0
+  
+  answer += 1
+
+print(answer)
