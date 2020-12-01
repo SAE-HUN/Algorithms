df4908585a0b813906ce7ba31d5310e0b4acf863
SAE-HUN <59135804+SAE-HUN@users.noreply.github.com>
2020-12-01 20:02:32 +0900

    우체국

--- Greedy/2141.py
+++ Greedy/2141.py
@@ -1,4 +1,3 @@
-import bisect
 import sys
 
 n = int(input())
@@ -6,12 +5,12 @@ total, towns = 0, []
 
 for _ in range(n):
     loc, mnt = map(int, sys.stdin.readline().split())
-    total+=mnt; towns.append([loc, mnt])
+    total+=mnt; towns.append((loc, mnt))
 
 stack = 0; towns.sort()
 
-for i, (loc, mnt) in enumerate(towns):
+for loc, mnt in towns:
     stack += mnt
-    towns[i][1] = stack
-
-print(towns[bisect.bisect_left([*map(lambda x:x[1], towns)], total/2)][0])
+    if stack>=total/2:
+        print(loc)
+        break
